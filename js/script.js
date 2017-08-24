27204552dbc6e64b80b20f3be9533ed4cb17823c
Singh <Yash_Kumar_Singh@Dell.com>
2017-08-24 11:11:59 +0530

    RepairedHeroSection

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
-	$('.parallax').parallax();
-	 $(".nav-wrapper").css("background-color", "#0a0c0d");
+	$('.parallax').parallax();	
 });
+
